6e03ab7474960be3819e1afae5dda4a775a738e2
Markus Rößler <roesslermarkus@gmail.com>
2023-12-20 13:18:42 +0100

    added IntegrationTest project for code coverage

---
+++ GithubSarifCollector.IntegrationTest/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace GithubSarifCollector.IntegrationTest;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
