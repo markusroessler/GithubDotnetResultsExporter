6f18567a5faa798e3569e2ef3590a8e27b8fe6ea
Markus Rößler <roesslermarkus@gmail.com>
2023-12-25 14:45:13 +0100

    #10 revert "format message as code"

--- GithubDotnetResultsExporter.Model.Test/GithubDotnetResultsExporterModelOpsTest.cs
+++ GithubDotnetResultsExporter.Model.Test/GithubDotnetResultsExporterModelOpsTest.cs
@@ -149,10 +149,10 @@ public class GithubDotnetResultsExporterModelOpsTest
             """
             ## Build Results
             :warning: [Foobar.cs#L1](https://github.com/markusroessler/GithubDotnetResultsExporter/blob/develop/project/Foobar.cs#L1)  
-            ```Warning Message```  
+            Warning Message  
 
             :x: [Blub.cs#L1](https://github.com/markusroessler/GithubDotnetResultsExporter/blob/develop/project/Blub.cs#L1)  
-            ```Error Message```  
+            Error Message  
 
 
             """));
