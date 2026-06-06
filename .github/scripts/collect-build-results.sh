fb9346b4a817bb4af0682e5dea45cb3c17f28e4e
mroessler <roesslermarkus@gmail.com>
2026-06-06 22:40:08 +0200

    added option to exclude build or test results

--- .github/scripts/collect-build-results.sh
+++ .github/scripts/collect-build-results.sh
@@ -2,11 +2,11 @@
 
 if [[ $1 == "use_dotnet_run" ]]; then 
     dotnet run --project GithubDotnetResultsExporter.Csl --framework net10.0 -- \
-        --export-step-summary true --github-server-url $2 --github-repo $3 --github-ref-name $4 --culture "de-DE"
+        --export-step-summary true --github-server-url $2 --github-repo $3 --github-ref-name $4 --step-summary-content-types $5 --culture "de-DE"
 
 elif [[ $1 == "use_installed" ]]; then
     export-dotnet-results-for-github \
-        --export-step-summary true --github-server-url $2 --github-repo $3 --github-ref-name $4 --culture "de-DE"
+        --export-step-summary true --github-server-url $2 --github-repo $3 --github-ref-name $4 --step-summary-content-types $5 --culture "de-DE"
 
 else
     echo "invalid arg: $1"
